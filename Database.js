6b142092b9a6f36724b8643317ecf0fd67b6aabc
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2023-03-20 00:32:48 +0530

    vercel changes

--- Database.js
+++ Database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const mongoURI = process.env.mongoURI
 
 const connectToMongo = () => {
-    console.log(mongoURI, typeof mongoURI)
     mongoose.connect(String(mongoURI)).then(() => {
         console.log('connect to MongoDb successfully')
     }).catch((err) => {
